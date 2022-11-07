53cb3e914f7385571c36fa2fa846636c71abb002
Valerii GANTCEV <4vpost@gmail.com>
2022-11-07 17:10:20 +0500

    // Exception//метод, принимающий в качестве аргументов два целочисленных массива, и возвращающий новый массив, каждый элемент которого равен разности элементов двух входящих массивов в той же ячейке. Если длины массивов не равны, необходимо как-то оповестить пользователя. ,,Важно: При выполнении метода единственное исключение, которое пользователь может увидеть - RuntimeException, т.е. ваше.

---
+++ HW1JEx2.java
@@ -0,0 +1,22 @@
+package HW1JEx;
+
+// Посмотрите на код, и подумайте сколько разных типов исключений вы тут сможете получить?
+
+public class HW1JEx2 {
+    public static int sum2d(String[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < 5; j++) { // ArrayOutOfBoundExeption. The right line: (int j = 0; j < arr[i].length; j++)
+                int val = Integer.parseInt(arr[i][j]); // NumberFormatException. Make try/catch.
+                sum+=val;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
