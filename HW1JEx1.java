bbd669399e87f469b8f5806a05309477bd0a790b
Valerii GANTCEV <4vpost@gmail.com>
2022-11-07 14:34:18 +0500

    // Exception//Посмотрите на код, и подумайте сколько разных типов исключений вы тут сможете получить?

---
+++ HW1JEx1.java
@@ -0,0 +1,22 @@
+package HW1JEx;
+
+// Посмотрите на код, и подумайте сколько разных типов исключений вы тут сможетеполучить?
+
+public class HW1JEx1 {
+    public static int sum2d(String[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < 5; j++) { // ArrayOutOfBoundExeption. The right line: (int j = 0; j < arr[i].length; j++)
+                int val = Integer.parseInt(arr[i][j]); // NumberFormatException. Make try/catch.
+                sum+=val;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
